18742e4a785a169fb49d5f99a1074a6110ed207a
Iván Gulin <ivanga96@educastur.es>
2024-04-16 01:12:35 +0200

    Refactorizando el código llevandolo a una clase

--- Program.cs
+++ Program.cs
@@ -8,51 +8,8 @@
         System.Console.WriteLine("Escribe la cantidad de delay para escribir.");
         int delay = Convert.ToInt32(Console.ReadLine());
 
-        
-        
-        // Crear una lista para almacenar los caracteres alfanuméricos españoles en UTF-8
-        List<char> caracteresEspañoles = new List<char>();
-        caracteresEspañoles.Add('ñ');
-        caracteresEspañoles.Add(' ');
-        
-        // Agregar letras del alfabeto latino básico (mayúsculas y minúsculas)
-        for (char letra = 'A'; letra <= 'Z'; letra++)
-        {
-            caracteresEspañoles.Add(letra);
-            caracteresEspañoles.Add(char.ToLower(letra));
-        }
+        Pajarito pajarito= new Pajarito();
 
-
-        // Agregar dígitos del 0 al 9
-        for (char digito = '0'; digito <= '9'; digito++)
-        {
-            caracteresEspañoles.Add(digito);
-        }
-
-        void EscribirDelay(String frase, int delay)
-        {
-            char letra = ' ';
-            String fraseActual = "";
-            int pos = 0;
-
-            for (int i = 0; frase != fraseActual; i++)
-            {
-                if (i >= caracteresEspañoles.Count)
-                {
-                    i = 0;
-                }
-                if (frase[pos] == letra)
-                {
-                    fraseActual += letra;
-                    pos++;
-                }
-                if (frase == fraseActual) System.Console.WriteLine(fraseActual);
-                else Console.WriteLine(fraseActual + letra);
-                
-                letra = caracteresEspañoles[i];
-                Thread.Sleep(delay);
-            }
-        }
-        EscribirDelay(frase, delay);
+        pajarito.EscribirDelay(frase, delay);
     }
 }
